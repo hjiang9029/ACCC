4269b3e84b1a2357daccf617db9483c51498c09c
Henry (Local) <henryjiang@live.ca>
2019-02-11 14:08:02 -0800

    Milestone 1

--- app/src/main/java/com/github/hjiang9029/accc/MainActivity.java
+++ app/src/main/java/com/github/hjiang9029/accc/MainActivity.java
@@ -1,20 +1,124 @@
 package com.github.hjiang9029.accc;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.*;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+    private ListView lv;
+    // URL to get contacts JSON
+    private static String SERVICE_URL = "http://opendata.newwestcity.ca/downloads/parks/PARKS.json";
+    ArrayList<String> parkNames = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.nameList);
+        new GetContacts().execute();
     }
 
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(SERVICE_URL);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    //JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONObject parkJsonObj = new JSONObject(jsonStr);
+                    JSONArray parkJsonArray = parkJsonObj.getJSONArray("features");
+                    // looping through All Contacts
+                    for (int i = 0; i < parkJsonArray.length(); i++) {
+                        JSONObject c = parkJsonArray.getJSONObject(i);
+                        JSONObject props = c.getJSONObject("properties");
+                        String name = props.getString("Name");
+                        parkNames.add(name);
+
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+
+            //Toon[] toonArray = toonList.toArray(new Toon[toonList.size()]);
+
+            ArrayAdapter<String> strAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, parkNames);
+
+            // Attach the adapter to a ListView
+            lv.setAdapter(strAdapter);
+        }
+    }
+
+
     public void createMap(View view) {
-        Intent i = new Intent(this, MapsActivity.class);
+        Intent i = new Intent(this, NavActivity.class);
         startActivity(i);
     }
 
