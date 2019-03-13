4df83442a681b105a174ce0216e6db73ea4e286e
Henry (Local) <henryjiang@live.ca>
2019-03-13 16:00:15 -0700

    Added hardcode routing
    Creates a route from point A to point B, using 'hardcoded' values

--- app/src/main/java/com/github/hjiang9029/accc/MainActivity.java
+++ app/src/main/java/com/github/hjiang9029/accc/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv = (ListView) findViewById(R.id.nameList);
+        //lv = (ListView) findViewById(R.id.nameList);
         new GetContacts().execute();
     }
 
